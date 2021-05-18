68471dcc7e156c41636c015523956d87c9aaf99f
Abdullah-irfan <83572170+Abdullah-irfan@users.noreply.github.com>
2021-05-18 19:37:18 +0530

    q&stack
    q&stack

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace q_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            q.qq();
+            Console.WriteLine();
+            stack.abstack();
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ q.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace q_stack
+{
+   public class q
+    {
+        public static void qq()
+        {
+            Queue irfan = new Queue();
+            irfan.Enqueue("welcome");
+            irfan.Enqueue(20);
+            irfan.Enqueue("abdullah");
+            Console.WriteLine("number of iteam:{0}", irfan.Count);
+
+            foreach (var item in irfan)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("number of iteam:{0}\n", irfan.Count);
+            Console.WriteLine("value:{0}", irfan.Contains(20));
+            Console.WriteLine("peek value is:{0}",irfan.Peek());
+
+
+        }
+    }
+    public class stack
+    {
+        public static void abstack()
+        {
+            Stack abstack = new Stack();
+            abstack.Push("welcome abdullah");
+            abstack.Push("hi");
+            abstack.Push(30);
+
+            foreach (var item in abstack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("number of iteam:{0}\n", abstack.Count);
+            abstack.Pop();
+            Console.WriteLine(abstack);
+            foreach (var item in abstack)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("value:{0}", abstack.Contains("hi"));
+        }
+    }
+}
